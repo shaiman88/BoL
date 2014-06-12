98bec39003de049399e1c724e8ba216892244b02
Feez <dontmesswithfeez@gmail.com>
2014-06-12 14:03:46 -0500

    [2.1]

--- Fiddlesticks Helper.lua
+++ Fiddlesticks Helper.lua
@@ -1,9 +1,9 @@
 if myHero.charName ~= "FiddleSticks" then return end
 
 require 'VPrediction'
-require 'SOW'
+require 'SourceLib'
 
-local version = '2.0'
+local version = '2.1'
 local QRange = 600 -- (actually 575)
 local WRange = 570 -- (actually 575)
 local ERange = 775 -- (actually 750)
@@ -30,10 +30,10 @@ function OnLoad()
 	FHConfig:addSubMenu("KS Settings", "kssettings")
 
 	FHConfig.drawsettings:addParam("enabledraw", "Enable Draw", SCRIPT_PARAM_ONOFF, true)
-	FHConfig.drawsettings:addParam("qrange", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
-	FHConfig.drawsettings:addParam("wrange", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
-	FHConfig.drawsettings:addParam("erange", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
-	FHConfig.drawsettings:addParam("rrange", "Draw R Range", SCRIPT_PARAM_ONOFF, true)
+	FHConfig.drawsettings:addParam("qdraw", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
+	FHConfig.drawsettings:addParam("wdraw", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
+	FHConfig.drawsettings:addParam("edraw", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
+	FHConfig.drawsettings:addParam("rdraw", "Draw R Range", SCRIPT_PARAM_ONOFF, true)
 
 	FHConfig.combosettings:addParam("comboActive", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
 
@@ -46,7 +46,7 @@ function OnLoad()
 	
 	FHConfig:addParam("SAC", "Using SAC or MMA", SCRIPT_PARAM_ONOFF, false)
 
-
+	PacketHandler:HookOutgoingPacket(Packet.headers.S_MOVE, CancelMovement)
 
 	ts = TargetSelector(TARGET_LESS_CAST_PRIORITY,750,DAMAGE_MAGIC)
 	ts.name = "FiddleSticks"
@@ -60,16 +60,16 @@ end
 function OnDraw()
 	if FHConfig.drawsettings.enabledraw then
 		if FHConfig.drawsettings.qdraw then
-			DrawCircle(myHero.x, myHero.y, myHero.z, QRange, ARGB(255,36,0,255))
+			DrawCircle2(myHero.x, myHero.y, myHero.z, QRange, ARGB(255,36,0,255))
 		end
 		if FHConfig.drawsettings.wdraw then
-			DrawCircle(myHero.x, myHero.y, myHero.z, WRange, ARGB(255,216,0,255))
+			DrawCircle2(myHero.x, myHero.y, myHero.z, WRange, ARGB(255,216,0,255))
 		end
 		if FHConfig.drawsettings.edraw then
-			DrawCircle(myHero.x, myHero.y, myHero.z, ERange, ARGB(255,66,0,66))
+			DrawCircle2(myHero.x, myHero.y, myHero.z, ERange, ARGB(255,66,0,66))
 		end
 		if FHConfig.drawsettings.rdraw then
-			DrawCircle(myHero.x, myHero.y, myHero.z, 800, ARGB(255,211,44,44))
+			DrawCircle2(myHero.x, myHero.y, myHero.z, 800, ARGB(255,211,44,44))
 		end
 	end
 end
@@ -87,6 +87,8 @@ function OnTick()
 	bftslot = GetInventorySlotItem(3188)
 	bftready = (bftslot ~= nil and myHero:CanUseSpell(bftslot) == READY)
 
+	if (IsKeyDown(GetKey("W")) or IsKeyDown(GetKey("w"))) and wready then Draining = true end
+
 	if ultActive and ts.target ~= nil then
 		if (dfgready or bftready) and FHConfig.itemsettings.dfg and ValidTarget(ts.target, 750) then
 			if dfgready then CastExploit(dfgslot, ts.target) end
@@ -100,7 +102,7 @@ function OnTick()
 	if Draining and _G.AutoCarry and _G.AutoCarry.MyHero then
 		_G.AutoCarry.MyHero:AttacksEnabled(false)
 		_G.AutoCarry.MyHero:MovementEnabled(false)
-	else 
+	elseif _G.AutoCarry and _G.AutoCarry.MyHero then
 		_G.AutoCarry.MyHero:AttacksEnabled(true) 
 		_G.AutoCarry.MyHero:MovementEnabled(true)
 	end
@@ -111,12 +113,11 @@ function OnTick()
 end
 
 
-function OnSendPacket(packet)
-	if packet.header == 'S_CAST' then 
-   		if p:get("spellId") == _W then
-   			Draining = true
-   		end
-   end
+function CancelMovement(p)
+	if Draining and FHConfig.combosettings.comboActive then
+		local packet = Packet(p)
+		p:Block()
+	end
 end
 
 function AutoKS()
@@ -145,17 +146,16 @@ end
 
 
 function Combo()
-	if FHConfig.combosettings.comboActive and ts.target ~= nil then
-		if qready and ValidTarget(ts.target, 570) and not Draining then 
+	if FHConfig.combosettings.comboActive and ts.target ~= nil  and not Draining then
+		if qready and ValidTarget(ts.target, 570) then 
 			CastExploit(_Q, ts.target)
 		end
 
-		if Draining then
-		elseif wready and not Draining and ValidTarget(ts.target, 570) then
+		if wready and not Draining and ValidTarget(ts.target, 570) then
 			CastExploit(_W, ts.target)
 		end
 
-		if not Draining and eready and ValidTarget(ts.target, 750) then
+		if eready and ValidTarget(ts.target, 750) then
 			CastExploit(_E, ts.target)
 		end
 	end
@@ -203,5 +203,28 @@ function OnLoseBuff(unit, buff)
 	end
 	if unit.isMe and buff.name == "Crowstorm" then
 		ultActive = false
+	end
+end
+
+function DrawCircleNextLvl(x, y, z, radius, width, color, chordlength)
+	radius = radius or 300
+	quality = math.max(8,math.floor(180/math.deg((math.asin((chordlength/(2*radius)))))))
+	quality = 2 * math.pi / quality
+	radius = radius*.92
+	local points = {}
+	for theta = 0, 2 * math.pi + quality, quality do
+		local c = WorldToScreen(D3DXVECTOR3(x + radius * math.cos(theta), y, z - radius * math.sin(theta)))
+		points[#points + 1] = D3DXVECTOR2(c.x, c.y)
+	end
+	DrawLines2(points, width or 1, color or 4294967295)
+end
+
+function DrawCircle2(x, y, z, radius, color)
+	local vPos1 = Vector(x, y, z)
+	local vPos2 = Vector(cameraPos.x, cameraPos.y, cameraPos.z)
+	local tPos = vPos1 - (vPos1 - vPos2):normalized() * radius
+	local sPos = WorldToScreen(D3DXVECTOR3(tPos.x, tPos.y, tPos.z))
+	if OnScreen({ x = sPos.x, y = sPos.y }, { x = sPos.x, y = sPos.y })  then
+		DrawCircleNextLvl(x, y, z, radius, 1, color, 75)	
 	end
 end
